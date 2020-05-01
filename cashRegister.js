b2c188a114296c29ff292fc504066e924a763ebd
Yacob Abusadah <50770569+KonoPowa@users.noreply.github.com>
2020-05-01 16:04:24 -0500

    Update cashRegister.js

--- cashRegister.js
+++ cashRegister.js
@@ -7,6 +7,7 @@ function checkCashRegister(price, cash, cid) {
     var multiplier = Math.pow(10, precision || 0);
     return Math.round(value * multiplier) / multiplier;
 }
+//added numerical value at index [0]
   let answer = [ 
   [.01,"PENNY", 0],
   [.05,"NICKEL", 0],
@@ -20,16 +21,20 @@ function checkCashRegister(price, cash, cid) {
   ]
   //change due
   let extra = cash - price
+
   for(let i = 8; i > -1; i--){
     while (extra > 0){
       if(extra - answer[i][0] > -.01 & cid[i][1] > 0){
+		  //adds coin value to answer[i][2], subtracts coin value from extra and cid
           answer[i][2] += answer[i][0]
           extra -= answer[i][0]
           cid[i][1] -= answer[i][0]
+		  //checks for insufficient funds, intentionally added before checking for closed due to what if looks for
           if(cid[0][1] + cid[1][1] + cid[2][1] == 0 & extra > 0){
         return {status: "INSUFFICIENT_FUNDS", change: []}
         break;
         }
+		//checks for closed, if closed I calculated the change within this block and format it as necessary
           if (cid[0][1] + cid[1][1] + cid[2][1] + cid[3][1] + cid[4][1] <= 0){
             answer = answer.filter(curr => curr[2] > 0)
             for (let i = 0; i < answer.length; i++){
@@ -43,11 +48,14 @@ function checkCashRegister(price, cash, cid) {
             return {status: "CLOSED", change: answer.concat(cid)}
           }
       }
+	  //if not closed or insufficient, the loop breaks and proceeds to calculate change and formats below
       else{
         break;
       }
   }
 }
+
+//change calculation and formatting
 answer = answer.filter(curr => curr[2] > 0).reverse()
 for (let i = 0; i < answer.length; i++){
   answer[i].splice(0,1)
